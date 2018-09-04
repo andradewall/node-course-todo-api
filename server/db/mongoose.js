d11a40f9a4f1b9056d02b949a88752c5f8f980b6
wallace <wallace@ime.eb.br>
2018-09-04 15:47:46 -0300

    Updated mongolab credentials

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,10 +1,15 @@
 var mongoose = require('mongoose');
-var dbuser = 'admin';
-var dbpassword = 'admin1'
+
+let dbuser = 'admin';
+let dbpassword = 'admin1'
+
+let db {
+   mlab: `mongodb://${dbuser}:${dbpassword}@ds245532.mlab.com:45532/todoapp`
+   localhost: 'mongodb://localhost:27017/TodoApp'
+}
 
 mongoose.Promise = global.Promise;
-// mongoose.connect('mongodb://localhost:27017/TodoApp');
-mongoose.connect(`mongodb://${dbuser}:${dbpassword}@ds245532.mlab.com:45532/todoapp`);
+mongoose.connect(process.env.PORT ? db.localhost : db.mlab);
 
 // Exporting var mongoose
 module.exports = {mongoose};
