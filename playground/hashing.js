94a7717b69305a12290e5fbac9d88010be89b448
wallace <wallace@ime.eb.br>
2018-10-16 15:37:25 -0300

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,32 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-   id: 10
-}
+var password = '123abc!';
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+// bcrypt.genSalt(10, (err, salt) => {
+//    bcrypt.hash(password, salt, (err, hash) => {
+//       console.log(hash);
+//    });
+// });
 
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded', decoded);
+var hashedPassword = '$2a$10$DPZciZB7K6oIo2JLcrNUQ.CtjGj9zljobgTKKSpeqZEp7UH3g22Dy';
+
+bcrypt.compare('password', hashedPassword, (err, res) => {
+   console.log(res);
+});
+
+
+
+// var data = {
+//    id: 10
+// }
+
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+
+// var decoded = jwt.verify(token, '123abc');
+// console.log('decoded', decoded);
 // jwt.verify
 
 // var message = 'I am user number 3';
--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
 var UserSchema = new mongoose.Schema({
 	email: {
@@ -67,6 +68,25 @@ UserSchema.statics.findByToken = function (token) {
 		'tokens.access': 'auth'
 	})
 };
+
+// Roda o código antes do evento informado ocorrer
+UserSchema.pre('save', function (next) {
+	var user = this;
+
+	if (user.isModified('password')) {
+		// user.password
+		bcrypt.genSalt(10, (err, salt) => {
+			bcrypt.hash(user.password, salt, (err, hash) => {
+				user.password = hash;
+				next();
+			});
+		});
+		// user.password = hash
+		// next();
+	} else {
+		next();
+	}
+});
 
 // Setting the fields. The first param from model() is the name of class and will be the name of the collection in plural
 var User = mongoose.model('User', UserSchema);
