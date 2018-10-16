da93f58bea4a40dce7e02728d56ad4609d0df59c
wallace <wallace@ime.eb.br>
2018-10-16 07:50:39 -0300

    Add POST /users route and token method

--- server/models/user.js
+++ server/models/user.js
@@ -1,13 +1,57 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
-// Setting the fields. The first param from model() is the name of class and will be the name of the collection in plural
-var User = mongoose.model('User', {
+var UserSchema = new mongoose.Schema({
 	email: {
 		type: String,
 		minlength: 1,
 		required: true,
-		trim: true
-	}
+		trim: true,
+		unique: true,
+		validate: {
+			validator: validator.isEmail,
+			message: '{VALUE} is not a valid email'
+		}
+	},
+	password: {
+		type: String,
+		required: true,
+		minlength: 6
+	},
+	tokens: [{
+		access: {
+			type: String,
+			required: true
+		},
+		token: {
+			type: String,
+			required: true
+		}
+	}]
 });
+
+UserSchema.methods.toJSON = function () {
+	var user = this;
+	var userObject = user.toObject();
+
+	return _.pick(userObject, ['_id', 'email']);
+};
+
+UserSchema.methods.generateAuthToken = function () {
+	var user = this;
+	var access = 'auth';
+	var token = jwt.sign({_id: user._id.toHexString()}, 'abc123').toString();
+
+	user.tokens = user.tokens.concat({access, token});
+	
+	return user.save().then(() => {
+		return token;
+	});
+};
+
+// Setting the fields. The first param from model() is the name of class and will be the name of the collection in plural
+var User = mongoose.model('User', UserSchema);
 
 module.exports = {User};
