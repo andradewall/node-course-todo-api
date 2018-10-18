5f0664b633a5721067936ea116adea8d82711d0e
wallace <wallace@ime.eb.br>
2018-10-18 13:22:44 -0300

    Update config to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,15 +1,17 @@
 var env = process.env.NODE_ENV || 'development';
 
+if (env === 'development' || env === 'test') {
+	var config = require('./config.json');
+	var envConfig = config[env];
+	
+	Object.keys(envConfig).forEach((key) => {
+		process.env[key] = envConfig[key];
+	});
+}
+
 let dbuser = 'admin';
 let dbpassword = 'admin1'
 
-
-if (env === 'development') {
-	process.env.PORT = 3000;
-	process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-} else if (env === 'test') {
-	process.env.PORT = 3000;
-	process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
-} else if (env === 'production') {
+if (env === 'production') {
    process.env.MONGODB_URI =  `mongodb://${dbuser}:${dbpassword}@ds245532.mlab.com:45532/todoapp`;
 }
